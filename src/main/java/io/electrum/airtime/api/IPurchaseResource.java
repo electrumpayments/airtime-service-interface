55fc236c0d538c1fbdddec9b85b1b7b65463c78c
gevashkar <gevashkar.rampersadh@electrum.co.za>
2020-08-24 10:40:33 +0200

    trial purchase

--- src/main/java/io/electrum/airtime/api/IPurchaseResource.java
+++ src/main/java/io/electrum/airtime/api/IPurchaseResource.java
@@ -1,6 +1,7 @@
 package io.electrum.airtime.api;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Request;
@@ -29,6 +30,29 @@ public interface IPurchaseResource {
          AsyncResponse asyncResponse,
          UriInfo uriInfo,
          HttpServletRequest httpServletRequest);
+
+   /**
+    *
+    * @since v5.15.0
+    *
+    * @param body
+    * @param securityContext
+    * @param request
+    * @param httpHeaders
+    * @param asyncResponse
+    * @param uriInfo
+    * @param httpServletRequest
+    */
+   default void trialPurchase(
+         PurchaseRequest body,
+         SecurityContext securityContext,
+         Request request,
+         HttpHeaders httpHeaders,
+         AsyncResponse asyncResponse,
+         UriInfo uriInfo,
+         HttpServletRequest httpServletRequest) {
+      asyncResponse.resume(new ServerErrorException("This operation has not been implemented.", 501));
+   }
 
    void reversePurchase(
          PurchaseReversal body,
