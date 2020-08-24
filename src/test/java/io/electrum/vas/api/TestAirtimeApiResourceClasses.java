49b4e72b450718ec0736f400caea06877c763d4b
gevashkar <gevashkar.rampersadh@electrum.co.za>
2020-08-24 10:54:19 +0200

    Update TestAirtimeApiResourceClasses.java

--- src/test/java/io/electrum/vas/api/TestAirtimeApiResourceClasses.java
+++ src/test/java/io/electrum/vas/api/TestAirtimeApiResourceClasses.java
@@ -1,27 +1,35 @@
 package io.electrum.vas.api;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import io.electrum.airtime.api.IPurchaseResource;
+
 public class TestAirtimeApiResourceClasses {
 
-    @Test(description = "Ensure that methods in the resource classes which define operations will ultimately call back to the original interface method.", dataProvider = "resourceInterfaceMethodsDataProvider")
-    public void ensureResourceOperationMethodBackwardsCompatibility(
-            Class<?> resourceClass,
-            String operationMethodName,
-            Class<?> interfaceClass,
-            String interfaceMethodName)
-            throws Exception {
-        TestResourceClasses.ensureResourceOperationMethodBackwardsCompatibility(
-                resourceClass,
-                operationMethodName,
-                interfaceClass,
-                interfaceMethodName);
-    }
+   @Test(description = "Ensure that methods in the resource classes which define operations will ultimately call back to the original interface method.", dataProvider = "resourceInterfaceMethodsDataProvider")
+   public void ensureResourceOperationMethodBackwardsCompatibility(
+         Class<?> resourceClass,
+         String operationMethodName,
+         Class<?> interfaceClass,
+         String interfaceMethodName)
+         throws Exception {
+      TestResourceClasses.ensureResourceOperationMethodBackwardsCompatibility(
+            resourceClass,
+            operationMethodName,
+            interfaceClass,
+            interfaceMethodName);
+   }
 
-    @DataProvider(name = "resourceInterfaceMethodsDataProvider")
-    public Object[][] resourceInterfaceMethodsDataProvider() {
-        return new Object[][] {
-                //@formatter:off
+   @DataProvider(name = "resourceInterfaceMethodsDataProvider")
+   public Object[][] resourceInterfaceMethodsDataProvider() {
+      return new Object[][] {
+            //@formatter:off
                 {MsisdnResourceTestClass.class, "lookupMsisdn", IMsisdnResourceTestImpl.class, "lookupMsisdn"},
                 {PurchaseResourceTestClass.class, "confirmPurchase", IPurchaseResourceTestImpl.class, "confirmPurchase"},
                 {PurchaseResourceTestClass.class, "purchasePurchase", IPurchaseResourceTestImpl.class, "purchasePurchase"},
@@ -31,7 +39,20 @@ public class TestAirtimeApiResourceClasses {
                 {VourcherResourceTestClass.class, "provisionVoucherImpl", IVouchersResourceTestImpl.class, "confirmVoucherImpl"},
                 {VourcherResourceTestClass.class, "reverseVoucherImpl", IVouchersResourceTestImpl.class, "confirmVoucherImpl"},
                 //@formatter:on
-        };
-    }
-}
+      };
+   }
 
+   @Test
+   public void ensureTrialPurchaseExists() {
+      // Setup
+      PurchaseResourceTestClass purchaseResource = new PurchaseResourceTestClass();
+      IPurchaseResource iPurchaseResource = purchaseResource.getResourceImplementation();
+      doNothing().when(iPurchaseResource).trialPurchase(any(), any(), any(), any(), any(), any(), any());
+
+      // Test
+      purchaseResource.trialPurchase(null, null, null, null, null, null, null);
+
+      // Assert
+      verify(iPurchaseResource, times(1)).trialPurchase(any(), any(), any(), any(), any(), any(), any());
+   }
+}
